4607f791173f8d0369181fccd8b4a2ae3cc7e3db
PavelPshen <pshen-leti@mail.ru>
2025-05-19 07:42:58 +0300

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,5 +1,7 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
@@ -11,7 +13,17 @@ public class TelegramBotService {
         this.handler = handler;
     }
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through init.");
+    }
+
     public void receive(Content content) {
         handler.receive(content);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed now.");
     }
 }
