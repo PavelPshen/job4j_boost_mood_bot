12513f52f65f7e7bb5a1a29f7dd3dabb5cf6af50
PavelPshen <pshen-leti@mail.ru>
2025-05-21 07:07:56 +0300

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
@@ -2,11 +2,16 @@ package ru.job4j.bmb.services;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @Service
-public class BotCommandHandler {
+public class BotCommandHandler implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
     @PostConstruct
     public void init() {
         System.out.println("Bean is going through init.");
@@ -14,6 +19,20 @@ public class BotCommandHandler {
 
     void receive(Content content) {
         System.out.println(content);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        System.out.println("ApplicationContext set in ApplicationContextAwareExample");
+    }
+
+    public void displayAllBeanNames() {
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        System.out.println("Beans in ApplicationContext:");
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
     @PreDestroy
--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -4,10 +4,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @Service
-public class TelegramBotService {
+public class TelegramBotService implements ApplicationContextAware {
     private final BotCommandHandler handler;
+    private ApplicationContext applicationContext;
 
     public TelegramBotService(BotCommandHandler handler) {
         this.handler = handler;
@@ -20,6 +23,20 @@ public class TelegramBotService {
 
     public void receive(Content content) {
         handler.receive(content);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        System.out.println("ApplicationContext set in ApplicationContextAwareExample");
+    }
+
+    public void displayAllBeanNames() {
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        System.out.println("Beans in ApplicationContext:");
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
     @PreDestroy
