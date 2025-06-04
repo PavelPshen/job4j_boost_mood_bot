85fad1feaad45143500369d52f5ba49bdbed6bce
PavelPshen <pshen-leti@mail.ru>
2025-06-04 14:05:56 +0800

    Механизм рассылки уведомлений [#505297 #545318]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,22 +1,20 @@
 package ru.job4j.bmb.model;
 
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mb_user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
-
-    public User() {
-        this.id = 0L;
-        this.clientId = 0;
-        this.chatId = 0;
-    }
-
-    public User(long id, long clientId, long chatId) {
-        this.id = id;
-        this.clientId = clientId;
-        this.chatId = chatId;
-    }
 
     public void setId(long id) {
         this.id = id;
@@ -40,6 +38,23 @@ public class User {
 
     public long getChatId() {
         return chatId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
 
